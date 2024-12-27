abd4d1efd57ac6b5b6fc3bcad20b6384b727d17c
mSonne85 <mSonne85@mail.de>
2024-12-27 12:06:46 +0100

    Update and rename Entity.cs to Collections/Generic/Entity.cs

--- Collections/Generic/Entity.cs
+++ Collections/Generic/Entity.cs
@@ -1,50 +1,19 @@
-﻿//------------------------------------------------------------------------------------------------------------------------------------------------------------------
-// File: "Entity.cs" | Authors: "Sonne170" | Date: 10.11.2024 | Rev. Date: 10.12.2024 | Version: 1.0.0.0
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace vsBlx48.Collections.Generic
 {
-    // Why don't use KeyValuePair<TKey, TValue>...
-    //
-    // Inheritance: Object -> ValueType -> KeyValuePair<TKey, TValue>
-    // 
-    // ...at least over time, Microsoft should have implemented the IComparable,
-    // IComparable<T> and IEquatable<T> interfaces to improve performance when
-    // comparing two KeyValuePair<TKey, TValue> objects. If the underlying type
-    // derived from ValueType is a structure, field reflection is used by
-    // ValueType.Equals and ValueType.GetHashCode. This is significantly slower
-    // than the counterpart of Entity<TKey, TValue>. Especially the overwritten
-    // equals method benefits from this implementation.
-    //
-    // ...and why don't use Tuple<T1, T2, ...>
-    //
-    // the reason for this is relatively simple, in most cases structures
-    // offer a better performance than classes. Keep in mind that structures
-    // are designed to group different data into one data set, while classes
-    // are designed to handle data for further processing, editing, converting
-    // mutating and so on.
-
-
     /// <summary>
     /// Represents a non-readonly 2-entity, or pair.
     /// </summary>
     /// <typeparam name="T1">The type of the entity's first component.</typeparam>
     /// <typeparam name="T2">The type of the entity's second component.</typeparam>
-    [StructLayout(LayoutKind.Sequential), Serializable]
-    public struct Entity<T1, T2> : IComparable, IComparable<Entity<T1, T2>>, IEquatable<Entity<T1, T2>> /*where T1 : IComparable where T2 : IComparable*/
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Entity<T1, T2> : IComparable, IComparable<Entity<T1, T2>>, IEquatable<Entity<T1, T2>>
     {
-        // fields
-        // -----------------------------------------------------------------------------------------------
-
         T1 first;
         T2 second;
-
-        // properties
-        // -----------------------------------------------------------------------------------------------
 
         /// <summary>
         /// Gets or sets the value of the first component.
@@ -56,9 +25,6 @@ namespace vsBlx48.Collections.Generic
         /// </summary>
         public T2 Second { get => second; set => second = value; } // for release build, property as fast as field
 
-        // constructors
-        // -----------------------------------------------------------------------------------------------
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Entity{T1, T2}"/> structure.
         /// </summary>
@@ -69,9 +35,6 @@ namespace vsBlx48.Collections.Generic
             this.first = first;
             this.second = second;
         }
-
-        // comparability
-        // -----------------------------------------------------------------------------------------------
 
         public int CompareTo(object obj)
         {
@@ -107,16 +70,13 @@ namespace vsBlx48.Collections.Generic
                 case 1: return Comparer<T1>.Default.Compare(first, other.first);
                 case 2: return Comparer<T2>.Default.Compare(second, other.second);
 
-                default: return CompareTo(other); // component out of range...
+                default: return CompareTo(other);
             }
         }
 
-        // equality
-        // -----------------------------------------------------------------------------------------------
-
+        // override equals methods otherwise we call ValueType.Equals() which uses reflection
         public override bool Equals(object obj)
         {
-            // override this method otherwise we call ValueType.Equals() which uses reflection!
             if (obj is Entity<T1, T2> other)
             {
                 return Equals(other);
@@ -126,16 +86,6 @@ namespace vsBlx48.Collections.Generic
 
         public bool Equals(Entity<T1, T2> other)
         {
-            // override this method otherwise we call ValueType.Equals which uses reflection!
-
-            //if ((object)item1 == (object)other.item1
-            //    && (object)item2 == (object)other.item2) return true;
-
-            //if (item1 == null || other.item1 == null
-            //    || item2 == null || other.item2 == null) return false;
-
-            //return item1.Equals(other.item1) && item2.Equals(other.item2);
-
             return EqualityComparer<T1>.Default.Equals(first, other.first)
                 && EqualityComparer<T2>.Default.Equals(second, other.second);
         }
@@ -154,7 +104,7 @@ namespace vsBlx48.Collections.Generic
                 case 1: return EqualityComparer<T1>.Default.Equals(first, other.first);
                 case 2: return EqualityComparer<T2>.Default.Equals(second, other.second);
 
-                default: return Equals(other); // component out of range...
+                default: return Equals(other);
             }
         }
 
@@ -164,9 +114,6 @@ namespace vsBlx48.Collections.Generic
             int h1 = first.GetHashCode();
             int h2 = second.GetHashCode();
             return ((h1 << 5) + h1) ^ h2;
-
-            // uses ValueType.GetHashCode() which is significantly slower
-            //return base.GetHashCode();
         }
 
         public static bool operator == (Entity<T1, T2> left, Entity<T1, T2> right)
@@ -178,9 +125,6 @@ namespace vsBlx48.Collections.Generic
         {
             return !left.Equals(right);
         }
-
-        // type name
-        // -----------------------------------------------------------------------------------------------
 
         public override string ToString()
         {
@@ -195,18 +139,12 @@ namespace vsBlx48.Collections.Generic
     /// <typeparam name="T1">The type of the entity's first component.</typeparam>
     /// <typeparam name="T2">The type of the entity's second component.</typeparam>
     /// <typeparam name="T3">The type of the entity's third component.</typeparam>
-    [StructLayout(LayoutKind.Sequential), Serializable]
+    [StructLayout(LayoutKind.Sequential)]
     public struct Entity<T1, T2, T3> : IComparable, IComparable<Entity<T1, T2, T3>>, IEquatable<Entity<T1, T2, T3>>
     {
-        // fields
-        // -----------------------------------------------------------------------------------------------
-
         T1 first;
         T2 second;
         T3 third;
-
-        // properties
-        // -----------------------------------------------------------------------------------------------
 
         /// <summary>
         /// Gets or sets the value of the first component.
@@ -223,9 +161,6 @@ namespace vsBlx48.Collections.Generic
         /// </summary>
         public T3 Third { get => third; set => third = value; }
 
-        // constructors
-        // -----------------------------------------------------------------------------------------------
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Entity{T1, T2, T3}"/> structure.
         /// </summary>
@@ -238,9 +173,6 @@ namespace vsBlx48.Collections.Generic
             this.second = second;
             this.third = third;
         }
-
-        // comparability
-        // -----------------------------------------------------------------------------------------------
 
         public int CompareTo(object obj)
         {
@@ -284,9 +216,7 @@ namespace vsBlx48.Collections.Generic
             }
         }
 
-        // equality
-        // -----------------------------------------------------------------------------------------------
-
+        // override equals methods otherwise we call ValueType.Equals() which uses reflection
         public override bool Equals(object obj)
         {
             if (obj is Entity<T1, T2, T3> other)
@@ -298,15 +228,6 @@ namespace vsBlx48.Collections.Generic
 
         public bool Equals(Entity<T1, T2, T3> other)
         {
-            //if ((object)item1 == (object)other.item1 && (object)item2 == (object)other.item2
-            //    && (object)item3 == (object)other.item3) return true;
-
-            //if (item1 == null || other.item1 == null || item2 == null || other.item2 == null
-            //    || item3 == null || other.item3 == null) return false;
-
-            //return item1.Equals(other.item1) && item2.Equals(other.item2)
-            //    && item3.Equals(other.item3);
-
             return EqualityComparer<T1>.Default.Equals(first, other.first)
                 && EqualityComparer<T2>.Default.Equals(second, other.second)
                 && EqualityComparer<T3>.Default.Equals(third, other.third);
@@ -352,9 +273,6 @@ namespace vsBlx48.Collections.Generic
             return !left.Equals(right);
         }
 
-        // type name
-        // -----------------------------------------------------------------------------------------------
-
         public override string ToString()
         {
             return string.Format("[{0}, {1}, {2}]", first, second, third);
@@ -368,19 +286,13 @@ namespace vsBlx48.Collections.Generic
     /// <typeparam name="T2">The type of the entity's second component.</typeparam>
     /// <typeparam name="T3">The type of the entity's third component.</typeparam>
     /// <typeparam name="T3">The type of the entity's fourth component.</typeparam>
-    [StructLayout(LayoutKind.Sequential), Serializable]
+    [StructLayout(LayoutKind.Sequential)]
     public struct Entity<T1, T2, T3, T4> : IComparable, IComparable<Entity<T1, T2, T3, T4>>, IEquatable<Entity<T1, T2, T3, T4>>
     {
-        // fields
-        // -----------------------------------------------------------------------------------------------
-
         T1 first;
         T2 second;
         T3 third;
         T4 fourth;
-
-        // properties
-        // -----------------------------------------------------------------------------------------------
 
         /// <summary>
         /// Gets or sets the value of the first component.
@@ -402,9 +314,6 @@ namespace vsBlx48.Collections.Generic
         /// </summary>
         public T4 Item4 { get => fourth; set => fourth = value; }
 
-        // constructors
-        // -----------------------------------------------------------------------------------------------
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Entity{T1, T2, T3, T4}"/> structure.
         /// </summary>
@@ -419,9 +328,6 @@ namespace vsBlx48.Collections.Generic
             this.third = third;
             this.fourth = fourth;
         }
-
-        // comparability
-        // -----------------------------------------------------------------------------------------------
 
         public int CompareTo(object obj)
         {
@@ -469,9 +375,6 @@ namespace vsBlx48.Collections.Generic
             }
         }
 
-        // equality
-        // -----------------------------------------------------------------------------------------------
-
         public override bool Equals(object obj)
         {
             if (obj is Entity<T1, T2, T3, T4> other)
@@ -483,15 +386,6 @@ namespace vsBlx48.Collections.Generic
 
         public bool Equals(Entity<T1, T2, T3, T4> other)
         {
-            //if ((object)item1 == (object)other.item1 && (object)item2 == (object)other.item2
-            //    && (object)item3 == (object)other.item3 && (object)item4 == (object)other.item4) return true;
-
-            //if (item1 == null || other.item1 == null || item2 == null || other.item2 == null
-            //    || item3 == null || other.item3 == null || item4 == null || other.item4 == null) return false;
-
-            //return item1.Equals(other.item1) && item2.Equals(other.item2)
-            //    && item3.Equals(other.item3) && item4.Equals(other.item4);
-
             return EqualityComparer<T1>.Default.Equals(first, other.first)
                 && EqualityComparer<T2>.Default.Equals(second, other.second)
                 && EqualityComparer<T3>.Default.Equals(third, other.third)
@@ -523,13 +417,13 @@ namespace vsBlx48.Collections.Generic
             int h1 = first.GetHashCode();
             int h2 = second.GetHashCode();
 
-            h1 = ((h1 << 5) + h1) ^ h2; // item1 + item2
+            h1 = ((h1 << 5) + h1) ^ h2;
             h2 = third.GetHashCode();
 
-            h1 = ((h1 << 5) + h1) ^ h2; // (item12) + item3
+            h1 = ((h1 << 5) + h1) ^ h2;
             h2 = fourth.GetHashCode();
 
-            return ((h1 << 5) + h1) ^ h2; // ((item12)3) + item4
+            return ((h1 << 5) + h1) ^ h2;
         }
 
         public static bool operator == (Entity<T1, T2, T3, T4> left, Entity<T1, T2, T3, T4> right)
@@ -541,9 +435,6 @@ namespace vsBlx48.Collections.Generic
         {
             return !left.Equals(right);
         }
-
-        // type name
-        // -----------------------------------------------------------------------------------------------
 
         public override string ToString()
         {
@@ -559,20 +450,14 @@ namespace vsBlx48.Collections.Generic
     /// <typeparam name="T3">The type of the entity's third component.</typeparam>
     /// <typeparam name="T3">The type of the entity's fourth component.</typeparam>
     /// <typeparam name="T3">The type of the entity's fifth component.</typeparam>
-    [StructLayout(LayoutKind.Sequential), Serializable]
+    [StructLayout(LayoutKind.Sequential)]
     public struct Entity<T1, T2, T3, T4, T5> : IComparable, IComparable<Entity<T1, T2, T3, T4, T5>>, IEquatable<Entity<T1, T2, T3, T4, T5>>
     {
-        // fields
-        // -----------------------------------------------------------------------------------------------
-
         T1 first;
         T2 second;
         T3 third;
         T4 fourth;
         T5 fifth;
-
-        // properties
-        // -----------------------------------------------------------------------------------------------
 
         /// <summary>
         /// Gets or sets the value of the first component.
@@ -599,9 +484,6 @@ namespace vsBlx48.Collections.Generic
         /// </summary>
         public T5 Fifth { get => fifth; set => fifth = value; }
 
-        // constructors
-        // -----------------------------------------------------------------------------------------------
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Entity{T1, T2, T3, T4, T5}"/> structure.
         /// </summary>
@@ -618,9 +500,6 @@ namespace vsBlx48.Collections.Generic
             this.fourth = fourth;
             this.fifth = fifth;
         }
-
-        // comparability
-        // -----------------------------------------------------------------------------------------------
 
         public int CompareTo(object obj)
         {
@@ -672,9 +551,6 @@ namespace vsBlx48.Collections.Generic
             }
         }
 
-        // equality
-        // -----------------------------------------------------------------------------------------------
-
         public override bool Equals(object obj)
         {
             if (obj is Entity<T1, T2, T3, T4, T5> other)
@@ -719,16 +595,16 @@ namespace vsBlx48.Collections.Generic
             int h1 = first.GetHashCode();
             int h2 = second.GetHashCode();
 
-            h1 = ((h1 << 5) + h1) ^ h2; // item1 + item2
+            h1 = ((h1 << 5) + h1) ^ h2;
             h2 = third.GetHashCode();
 
-            h1 = ((h1 << 5) + h1) ^ h2; // (item12) + item3
+            h1 = ((h1 << 5) + h1) ^ h2;
             h2 = fourth.GetHashCode();
 
-            h1 = ((h1 << 5) + h1) ^ h2; // ((item12)3) + item4
+            h1 = ((h1 << 5) + h1) ^ h2;
             h2 = fifth.GetHashCode();
 
-            return ((h1 << 5) + h1) ^ h2; // (((item12)3)4) + item5
+            return ((h1 << 5) + h1) ^ h2;
         }
 
         public static bool operator == (Entity<T1, T2, T3, T4, T5> left, Entity<T1, T2, T3, T4, T5> right)
@@ -740,9 +616,6 @@ namespace vsBlx48.Collections.Generic
         {
             return !left.Equals(right);
         }
-
-        // type name
-        // -----------------------------------------------------------------------------------------------
 
         public override string ToString()
         {
